23f9332d39e745196658f1e16b3f6dd9d9e848d3
Nathan Yu <nyu@ibm.com>
2021-04-01 01:51:30 -0400

    ppc64le

--- image/service-available/:ssl-tools/download.sh
+++ image/service-available/:ssl-tools/download.sh
@@ -1,33 +1,5 @@
 #!/bin/bash -e
 
-UARCH=$(uname -m)
-echo "Architecture is ${UARCH}"
-
-case "${UARCH}" in
-    
-    "x86_64")
-        HOST_ARCH="amd64"
-    ;;
-    
-    "arm64" | "aarch64")
-        HOST_ARCH="arm64"
-    ;;
-    
-    "armv7l" | "armv6l" | "armhf")
-        HOST_ARCH="arm"
-    ;;
-    
-    "i386")
-        HOST_ARCH="386"
-    ;;
-    
-    *)
-        echo "Architecture not supported. Exiting."
-        exit 1
-    ;;
-esac
-
-echo "Going to use ${HOST_ARCH} cfssl binaries"
 
 # download curl and ca-certificate from apt-get if needed
 to_install=()
@@ -46,19 +18,14 @@ fi
 
 LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends openssl jq
 
-# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=923479
-if [[ "${HOST_ARCH}" == 'arm' ]]; then
-    LC_ALL=C DEBIAN_FRONTEND=noninteractive c_rehash
-fi
-
 echo "Download cfssl ..."
-echo "curl -o /usr/sbin/cfssl -SL https://github.com/osixia/cfssl/releases/download/1.4.1/cfssl_linux-${HOST_ARCH}"
-curl -o /usr/sbin/cfssl -SL "https://github.com/osixia/cfssl/releases/download/1.4.1/cfssl_linux-${HOST_ARCH}"
+echo "curl -o /usr/sbin/cfssl -SL https://raw.githubusercontent.com/nyu058/docker-light-baseimage/ppc64le/assets/cfssl"
+curl -o /usr/sbin/cfssl -SL "https://raw.githubusercontent.com/nyu058/docker-light-baseimage/ppc64le/assets/cfssl"
 chmod 700 /usr/sbin/cfssl
 
 echo "Download cfssljson ..."
-echo "curl -o /usr/sbin/cfssljson -SL https://github.com/osixia/cfssl/releases/download/1.4.1/cfssljson_linux-${HOST_ARCH}"
-curl -o /usr/sbin/cfssljson -SL "https://github.com/osixia/cfssl/releases/download/1.4.1/cfssljson_linux-${HOST_ARCH}"
+echo "curl -o /usr/sbin/cfssljson -SL https://raw.githubusercontent.com/nyu058/docker-light-baseimage/ppc64le/assets/cfssljson"
+curl -o /usr/sbin/cfssljson -SL "https://raw.githubusercontent.com/nyu058/docker-light-baseimage/ppc64le/assets/cfssljson"
 chmod 700 /usr/sbin/cfssljson
 
 echo "Project sources: https://github.com/cloudflare/cfssl"
